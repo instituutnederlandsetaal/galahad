6301835c20dee7eed60dec10b9ac2867ed7ebb15
Vincent Prins <vincent.prins@ivdnt.org>
2024-09-18 14:49:23 +0200

    Make swagger work on localhost too

--- server/src/main/kotlin/org/ivdnt/galahad/app/GalahadApplication.kt
+++ server/src/main/kotlin/org/ivdnt/galahad/app/GalahadApplication.kt
@@ -74,85 +74,88 @@ const val DOCUMENT_RAW_FILE_URL = "$DOCUMENT_URL/raw" // returns the blob of the
 
 var application_profile: String = System.getenv("spring.profiles.active") ?: "prod"
 fun String.runCommand(workingDir: File, timeout: Long = 60): String? {
-	try {
-		val parts = this.split("\\s".toRegex())
-		val proc = ProcessBuilder(*parts.toTypedArray())
-			.directory(workingDir)
-			.inheritIO()
-			.start()
+    try {
+        val parts = this.split("\\s".toRegex())
+        val proc = ProcessBuilder(*parts.toTypedArray())
+            .directory(workingDir)
+            .inheritIO()
+            .start()
 
-		proc.waitFor(timeout, TimeUnit.MINUTES)
-		return proc.inputStream.bufferedReader().readText()
-	} catch(e: IOException) {
-		e.printStackTrace()
-		return null
-	}
+        proc.waitFor(timeout, TimeUnit.MINUTES)
+        return proc.inputStream.bufferedReader().readText()
+    } catch (e: IOException) {
+        e.printStackTrace()
+        return null
+    }
 }
 
 @Configuration
 @ConfigurationProperties(prefix = "")
 class Config {
 
-	lateinit var workDir: String
+    lateinit var workDir: String
 
-	@Bean
-	fun getWorkingDirectory(): File {
-		return File( workDir )
-	}
+    @Bean
+    fun getWorkingDirectory(): File {
+        return File(workDir)
+    }
 
-	companion object {
-		fun galahadVersion(): String {
-			val versionStream = this::class.java.classLoader.getResource("version.yml")!!.openStream()
-			val versionProperties = Properties()
-			versionProperties.load(versionStream)
+    companion object {
+        fun galahadVersion(): String {
+            val versionStream = this::class.java.classLoader.getResource("version.yml")!!.openStream()
+            val versionProperties = Properties()
+            versionProperties.load(versionStream)
 			return versionProperties.getProperty("GITHUB_REF_NAME")
-		}
-	}
+        }
+    }
 }
 
 @ComponentScan("org.ivdnt.galahad")
 @SpringBootApplication
 class GalahadApplication {
-	@Bean
-	fun customOpenAPI(): OpenAPI {
-		return OpenAPI()
-			.components(Components())
-			.servers(listOf(Server().url("/galahad/api").description("Default Server URL")))
-			.info(
-				io.swagger.v3.oas.models.info.Info()
-					.title("GaLAHaD API")
-					.version(Config.galahadVersion())
-					.license( License().name("Apache 2.0").url("https://www.apache.org/licenses/"))
-					.description("Generating Linguistic Annotations for Historical Dutch")
-					.contact(Contact().name("GaLAHaD GitHub").url("https://github.com/inl/galahad"))
-			)
-	}
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        var api = OpenAPI()
+            .components(Components())
+            .info(
+                io.swagger.v3.oas.models.info.Info()
+                    .title("GaLAHaD API")
+                    .version(Config.galahadVersion())
+                    .license(License().name("Apache 2.0").url("https://www.apache.org/licenses/"))
+                    .description("Generating Linguistic Annotations for Historical Dutch")
+                    .contact(Contact().name("GaLAHaD GitHub").url("https://github.com/inl/galahad"))
+            )
+        if (application_profile.contains("prod")) {
+            api = api.servers(listOf(Server().url("/galahad/api").description("GaLAHaD API")))
+        }
+        return api
+    }
 }
 
 fun main(args: Array<String>) {
-	runApplication<GalahadApplication>(*args)
+    runApplication<GalahadApplication>(*args)
 }
 
 @RestController
 class ApplicationController : ErrorController, Logging {
 
-	@Autowired
-	private val request: HttpServletRequest? = null
+    @Autowired
+    private val request: HttpServletRequest? = null
 
-	@GetMapping( BASE_URL )
-	@CrossOrigin
-	fun getApplication(): ResponseEntity<Void> {
-		// Since we have nothing to show at this URL, we redirect to the API UI instead
+    @GetMapping(BASE_URL)
+    @CrossOrigin
+    fun getApplication(): ResponseEntity<Void> {
+        // Since we have nothing to show at this URL, we redirect to the API UI instead
 		logger.info( "Get root" )
 		return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(request?.contextPath + SWAGGER_API_URL)).build()
-	}
+    }
 
-	@GetMapping( "/user" )
-	@CrossOrigin
-	fun getUser(): User {
+    @GetMapping("/user")
+    @CrossOrigin
+    fun getUser(): User {
 		logger.info( "Get user" )
-		return User.getUserFromRequestOrThrow( request )
-	}
+        return User.getUserFromRequestOrThrow(request)
+    }
 
 	@GetMapping( BENCHMARKS_URL )
 	@CrossOrigin
@@ -162,31 +165,31 @@ class ApplicationController : ErrorController, Logging {
 		return if( file.exists() ) file.readBytes() else null
 	}
 
-	@GetMapping( VERSION_URL )
-	@CrossOrigin
+    @GetMapping(VERSION_URL)
+    @CrossOrigin
 	fun getVersion(): ByteArray {
 		logger.info( "Get version" )
 		return this::class.java.classLoader.getResourceAsStream("version.yml")!!.readBytes()
-	}
+    }
 
 
-	data class ErrorResponse(
-		@JsonProperty val statusCode: HttpStatus,
-		@JsonProperty val message: String,
-	)
+    data class ErrorResponse(
+        @JsonProperty val statusCode: HttpStatus,
+        @JsonProperty val message: String,
+    )
 
-	@RequestMapping("/error")
-	@Hidden
-	@CrossOrigin
-	fun handleError(request: HttpServletRequest): ErrorResponse {
-		val statusCode = HttpStatus.valueOf( request.getAttribute("jakarta.servlet.error.status_code") as Int? ?: 500 )
-		val exception = request.getAttribute("jakarta.servlet.error.exception") as Exception?
-		if (exception?.cause is FileUploadException) {
-			// The error message is good as is. No need to wrap it
-			return ErrorResponse( statusCode, (exception.cause as FileUploadException).message ?: "exception inception" )
-		}
-		return ErrorResponse( statusCode, "${if (exception == null) "N/A" else exception.message}")
-	}
+    @RequestMapping("/error")
+    @Hidden
+    @CrossOrigin
+    fun handleError(request: HttpServletRequest): ErrorResponse {
+        val statusCode = HttpStatus.valueOf(request.getAttribute("jakarta.servlet.error.status_code") as Int? ?: 500)
+        val exception = request.getAttribute("jakarta.servlet.error.exception") as Exception?
+        if (exception?.cause is FileUploadException) {
+            // The error message is good as is. No need to wrap it
+            return ErrorResponse(statusCode, (exception.cause as FileUploadException).message ?: "exception inception")
+        }
+        return ErrorResponse(statusCode, "${if (exception == null) "N/A" else exception.message}")
+    }
 
 }
 
@@ -194,45 +197,45 @@ class ApplicationController : ErrorController, Logging {
 @ConfigurationProperties(prefix = "spring.servlet.multipart")
 class MultipartConfig {
 
-	lateinit var maxFileSize: String
-	lateinit var maxRequestSize: String
+    lateinit var maxFileSize: String
+    lateinit var maxRequestSize: String
 
-	val maxFilesSizeAsBytes: Long
-		get() {
-			return toBytes(maxFileSize)
-		}
+    val maxFilesSizeAsBytes: Long
+        get() {
+            return toBytes(maxFileSize)
+        }
 
-	companion object {
-		fun toBytes(filesize: String?): Long {
-			var returnValue: Long = -1
-			val patt: Pattern = Pattern.compile("([\\d.]+)([GMK]B)", Pattern.CASE_INSENSITIVE)
-			val matcher: Matcher = patt.matcher(filesize)
-			val powerMap: MutableMap<String, Int> = HashMap()
-			powerMap["GB"] = 3
-			powerMap["MB"] = 2
-			powerMap["KB"] = 1
-			if (matcher.find()) {
-				val number: String = matcher.group(1)
-				val pow = powerMap[matcher.group(2).uppercase()]!!
-				var bytes = BigDecimal(number)
-				bytes = bytes.multiply(BigDecimal.valueOf(1024).pow(pow))
-				returnValue = bytes.longValueExact()
-			}
-			return returnValue
-		}
-	}
+    companion object {
+        fun toBytes(filesize: String?): Long {
+            var returnValue: Long = -1
+            val patt: Pattern = Pattern.compile("([\\d.]+)([GMK]B)", Pattern.CASE_INSENSITIVE)
+            val matcher: Matcher = patt.matcher(filesize)
+            val powerMap: MutableMap<String, Int> = HashMap()
+            powerMap["GB"] = 3
+            powerMap["MB"] = 2
+            powerMap["KB"] = 1
+            if (matcher.find()) {
+                val number: String = matcher.group(1)
+                val pow = powerMap[matcher.group(2).uppercase()]!!
+                var bytes = BigDecimal(number)
+                bytes = bytes.multiply(BigDecimal.valueOf(1024).pow(pow))
+                returnValue = bytes.longValueExact()
+            }
+            return returnValue
+        }
+    }
 }
 
 @Configuration
 class RequestLoggingFilterConfig {
-	@Bean
-	fun logFilter(): CommonsRequestLoggingFilter {
-		val filter = CommonsRequestLoggingFilter()
-		filter.setIncludeQueryString(true)
-		filter.setIncludePayload(true)
-		filter.setMaxPayloadLength(10000)
-		filter.setIncludeHeaders(true)
-		filter.setAfterMessagePrefix("REQUEST DATA : ")
-		return filter
-	}
+    @Bean
+    fun logFilter(): CommonsRequestLoggingFilter {
+        val filter = CommonsRequestLoggingFilter()
+        filter.setIncludeQueryString(true)
+        filter.setIncludePayload(true)
+        filter.setMaxPayloadLength(10000)
+        filter.setIncludeHeaders(true)
+        filter.setAfterMessagePrefix("REQUEST DATA : ")
+        return filter
+    }
 }
