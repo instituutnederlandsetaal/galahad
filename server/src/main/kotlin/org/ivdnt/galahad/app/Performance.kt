9ed4899a4370d058212894e4b6c01e9ac27ec42f
Vincent Prins <Prins@inl.loc>
2024-09-27 16:18:41 +0200

    Log duration of each webrequest

---
+++ server/src/main/kotlin/org/ivdnt/galahad/app/Performance.kt
@@ -0,0 +1,40 @@
+package org.ivdnt.galahad.app
+
+import jakarta.servlet.*
+import jakarta.servlet.annotation.WebFilter
+import jakarta.servlet.http.HttpServletRequest
+import org.apache.logging.log4j.kotlin.Logging
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import java.io.IOException
+import java.time.Duration
+import java.time.Instant
+
+@Component
+@WebFilter("/*")
+class Performance : Filter, Logging {
+    @Throws(ServletException::class)
+    override fun init(filterConfig: FilterConfig?) {
+        // empty
+    }
+
+    @Throws(IOException::class, ServletException::class)
+    override fun doFilter(req: ServletRequest, resp: ServletResponse?, chain: FilterChain) {
+        val start = Instant.now()
+        try {
+            chain.doFilter(req, resp)
+        } finally {
+            var url = (req as HttpServletRequest).requestURI
+            if (url != "/user") {
+                val finish = Instant.now()
+                val time: Long = Duration.between(start, finish).toMillis()
+                logger.info("in $time ms: $url")
+            }
+        }
+    }
+
+    override fun destroy() {
+        // empty
+    }
+}
