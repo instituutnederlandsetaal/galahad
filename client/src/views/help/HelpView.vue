f2b5753620ea2ae54b08a43a9b1e0853af2458d2
Vincent Prins <vincent.prins@ivdnt.org>
2024-11-20 17:32:31 +0100

    Added Detailed Evaluation Help

--- client/src/views/help/HelpView.vue
+++ client/src/views/help/HelpView.vue
@@ -2,7 +2,8 @@
 
   <GTabs class="level-2" :basePath="'/help'" :tabs="[
     { id: 'general', title: 'General' },
-    { id: 'formats', title: 'Formats' }
+    { id: 'evaluation', title: 'Detailed Evaluation Help' },
+    { id: 'formats', title: 'Formats' },
   ]" />
 
 </template>
---
+++ client/src/views/help/subviews/EvaluationExampleCorpus.vue
@@ -0,0 +1,120 @@
+<template>
+    <table class="compact">
+        <thead>
+            <tr>
+                <th>Token</th>
+                <th>Actual PoS</th>
+                <th>Predicted PoS</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>lopen (to run)</td>
+                <td>verb</td>
+                <td>noun</td>
+            </tr>
+            <tr>
+                <td>snel (fast)</td>
+                <td>adjective</td>
+                <td>adjective</td>
+            </tr>
+            <tr>
+                <td>hond (dog)</td>
+                <td>noun</td>
+                <td>noun</td>
+            </tr>
+            <tr>
+                <td>mooie (beautiful)</td>
+                <td>adjective</td>
+                <td>adjective</td>
+            </tr>
+            <tr>
+                <td>spring (jump)</td>
+                <td>verb</td>
+                <td>verb</td>
+            </tr>
+            <tr>
+                <td>katten (cats)</td>
+                <td>noun</td>
+                <td>noun</td>
+            </tr>
+            <tr>
+                <td>lui (lazy)</td>
+                <td>adjective</td>
+                <td>noun</td>
+            </tr>
+            <tr>
+                <td>vos (fox)</td>
+                <td>noun</td>
+                <td>noun</td>
+            </tr>
+            <tr>
+                <td>speelde (played)</td>
+                <td>verb</td>
+                <td>verb</td>
+            </tr>
+            <tr>
+                <td>blij (happy)</td>
+                <td>adjective</td>
+                <td>adjective</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<style scoped lang="scss">
+table {
+    border-collapse: collapse;
+    margin: 0 auto;
+    margin-top: 5px;
+    padding: 0;
+
+    caption {
+        font-size: 1.5em;
+        margin: .5em 0 .75em;
+    }
+
+    tr {
+        border: 1px solid var(--int-very-light-grey-hover);
+
+        &:nth-child(even) {
+            background: #fff;
+        }
+
+        &:nth-child(odd) {
+            background: var(--int-very-light-grey);
+        }
+    }
+
+    thead {
+        >tr {
+            background-color: var(--int-theme) !important;
+        }
+    }
+
+    th {
+        padding: .6em;
+        text-align: center;
+        font-size: .85em;
+        letter-spacing: .1em;
+        text-transform: uppercase;
+    }
+
+    td {
+        padding: .5em;
+        text-align: center;
+        min-width: 60px;
+    }
+
+    overflow-x: auto;
+}
+
+table.compact {
+
+    td,
+    th {
+        padding: .1em 2em;
+    }
+
+}
+</style>
