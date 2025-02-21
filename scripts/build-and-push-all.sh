3259ef0d875076544314aad1aab87638a3a2f27a
Vincent Prins <vincent.prins@ivdnt.org>
2025-02-21 10:24:56 +0100

    Simplified proxy
    no need for a custom image

--- scripts/build-and-push-all.sh
+++ scripts/build-and-push-all.sh
@@ -5,6 +5,5 @@
 
 echo "Will push galahad images with version <$VERSION_LABEL>. Set VERSION_LABEL to override this."
 
-docker push instituutnederlandsetaal/galahad-proxy:$VERSION_LABEL
 docker push instituutnederlandsetaal/galahad-server:$VERSION_LABEL
 docker push instituutnederlandsetaal/galahad-client:$VERSION_LABEL
