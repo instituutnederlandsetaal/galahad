6e1d3abd04712180c9f18a7dd897c1ed9b325647
Vincent Prins <vincent.prins@ivdnt.org>
2025-09-15 15:34:20 +0200

    Allow txt export for anyone

--- client/src/components/input/FileFormatInput.vue
+++ client/src/components/input/FileFormatInput.vue
@@ -7,21 +7,6 @@
 
 <script setup lang="ts">
 import stores from "@/stores"
-import { Format } from "@/types/documents"
-import type { SelectOption } from "@/types/ui/select"
-
 const { format } = storeToRefs(stores.useExport())
-const { user } = storeToRefs(stores.useUser())
-
-const options: SelectOption[] = [
-    { value: Format.CONLLU, text: "CoNLL-U (Universal Dependencies)" },
-    { value: Format.FOLIA, text: "FoLiA (Format for Linguistic Annotation)" },
-    { value: Format.NAF, text: "NAF (NLP Annotation Format) " },
-    { value: Format.TEI_P5, text: "TEI P5 (Text Encoding Initiative)" },
-    { value: Format.TSV, text: "TSV (Tab-separated values)" },
-]
-// Admins can also export txt.
-if (user.isAdmin) {
-    options.push({ value: Format.TXT, text: "TXT (Plain text)" })
-}
+const { options } = stores.useExport()
 </script>
--- client/src/stores/export.ts
+++ client/src/stores/export.ts
@@ -1,24 +1,27 @@
-// Libraries & stores
-
-// Types & API
 import * as API from "@/api/export"
 import * as Utils from "@/api/utils"
 import stores from "@/stores"
 import { plausible } from "@/ts/plausible"
-import type { Format } from "@/types/documents"
+import { Format } from "@/types/documents"
+import type { SelectOption } from "@/types/ui/select"
 
-/**
- * Used to download exported corpora.
- */
+/** Used to download exported corpora. */
 const useExport = defineStore("exportStore", () => {
     // Stores
     const corporaStore = stores.useCorpora()
-    const errors = stores.useErrors()
     const jobSelection = stores.useJobSelection()
 
     // Fields
     const loading = ref<boolean>()
     const format = ref<Format>()
+    const options: SelectOption[] = [
+        { value: Format.CONLLU, text: "CoNLL-U (Universal Dependencies)" },
+        { value: Format.FOLIA, text: "FoLiA (Format for Linguistic Annotation)" },
+        { value: Format.NAF, text: "NAF (NLP Annotation Format) " },
+        { value: Format.TEI_P5, text: "TEI P5 (Text Encoding Initiative)" },
+        { value: Format.TSV, text: "TSV (Tab-separated values)" },
+        { value: Format.TXT, text: "TXT (Plain text, tokens only)" },
+    ]
 
     // Methods
     function convert(shouldMerge: boolean, posHeadOnly: boolean): void {
@@ -30,7 +33,6 @@ const useExport = defineStore("exportStore", () => {
         plausible.corpusExported(corporaStore.corpus, jobSelection.hypothesisId, format.value, shouldMerge, posHeadOnly)
         API.convertCorpus(corporaStore.corpusId, jobSelection.hypothesisId, format.value, posHeadOnly)
             .then(Utils.browserDownloadResponseFile)
-            .catch((res) => Utils.handleBlobError(res, "convert corpus", errors))
             .finally(() => (loading.value = false))
     }
 
@@ -39,17 +41,10 @@ const useExport = defineStore("exportStore", () => {
         plausible.corpusExported(corporaStore.corpus, jobSelection.hypothesisId, format.value, true, posHeadOnly)
         API.mergeCorpus(corporaStore.corpusId, jobSelection.hypothesisId, format.value, posHeadOnly)
             .then(Utils.browserDownloadResponseFile)
-            .catch((res) => Utils.handleBlobError(res, "merge corpus", errors))
             .finally(() => (loading.value = false))
     }
-    // Exports
-    return {
-        // Fields
-        format,
-        loading,
-        // Methods
-        convert,
-    }
+
+    return { format, options, loading, convert }
 })
 
 export default useExport
